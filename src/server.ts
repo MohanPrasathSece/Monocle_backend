8efffc33dc8a637f00b4ec5dd9686cf0ffd5b157
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-01-08 10:02:35 +0530

    remove COEP header

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,6 @@ app.use(express.urlencoded({ extended: true }));
 // Security Headers
 app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     next();
 });
 
